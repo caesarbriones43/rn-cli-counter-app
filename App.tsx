cdcf30200b8a25c8abc13ad0d53ff62afa25f1bb
caesarbriones43 <caesarbriones43@gmail.com>
2021-06-26 12:15:09 -0500

    init counter-app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center'}}>
+      <Text style={{textAlign: 'center', fontSize: 40}}>Hola Mundo</Text>
+    </View>
+  );
+};
+
+export default App;
